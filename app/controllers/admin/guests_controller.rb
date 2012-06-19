1279dce1dd62bf8d5b33b35193416dc584aa3051
Henri Sack <hsack@abriva.net>
2012-06-19 12:33:21 +0100

    fixed sorting searching

--- app/controllers/admin/guests_controller.rb
+++ app/controllers/admin/guests_controller.rb
@@ -1,9 +1,10 @@
 module Admin
 class GuestsController < ApplicationController
   before_filter :require_admin
+  helper_method :sort_column, :sort_direction
 
   def index
-    scope = params[:join] ? Guest.joins(params[:join]).order("#{params[:join]}.#{params[:sort]}") : Guest.order(params[:sort])
+    scope = params[:join] ? Guest.joins(params[:join]).order("#{params[:join]}.#{params[:sort]}") : Guest.order(sort_column + " " + sort_direction)
     @guests=scope.search(params[:search]).page(params[:page]).per(10)
   end
   
@@ -21,6 +22,16 @@ class GuestsController < ApplicationController
         format.html { redirect_to guests_url }
         format.json { head :no_content }
     end
+  end
+
+private
+  
+  def sort_column
+    params[:sort] || "name"
+  end
+
+  def sort_direction
+    params[:direction] || "asc"
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,8 @@ module ApplicationHelper
   def sortable(col, title = nil)
     column = col.to_s
     title ||= column.titleize
-#    css_class = column == sort_column ? "current #{sort_direction}" : nil
-#    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-#    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
-    link_to title, :sort => column
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, params.merge(sort: column, direction: direction, page: nil), {class: css_class}
   end
 end
