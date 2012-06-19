ffb2eadc30541bc70e98666cbc5c5c755d3c7fd5
Henri Sack <hsack@abriva.net>
2012-06-19 11:39:13 +0100

    fixed bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,9 @@ module ApplicationHelper
   def sortable(col, title = nil)
     column = col.to_s
     title ||= column.titleize
-    css_class = column == sort_column ? "current #{sort_direction}" : nil
-    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+#    css_class = column == sort_column ? "current #{sort_direction}" : nil
+#    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+#    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+    link_to title, :sort => column
   end
 end
