aaf0e462f63a959e9777e762ed699e0fc1432d8f
Henri Sack <hsack@abriva.net>
2012-07-03 07:30:52 +0100

    fixed signle photo import

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -5,7 +5,4 @@ CarrierWave.configure do |config|
     :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
   config.fog_directory  = 'chantal-et-henri.photos'                     # required
-#  config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
-#  config.fog_public     = false                                   # optional, defaults to true
-#  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
